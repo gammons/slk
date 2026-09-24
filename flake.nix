5537f3b603561be8991818f290ddf81fef161c77
Grant Ammons <grant@grant.dev>
2026-09-24 14:55:22 +0000

    test: deliberately stale vendorHash (DO NOT MERGE)
    Exercises nix-vendor-hash.yml's auto-push path end to end now that NIX_HASH_PUSH_TOKEN is configured. Uses nixpkgs' lib.fakeHash. This PR will be closed, not merged.

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
           pname = "slk";
           version = "0.0.0";
           src = ./.;
-          vendorHash = "sha256-/J4gr4m9v6Y0Be8BU4wepIdl2sjoPh0pFCvJL2kIeLk=";
+          vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
           buildInputs = [pkgs.libX11];
         };
       in {
